50212f2c4e66b7b214e10ec4a5fd7af4b1237d78
Joao Paulo Freire <jpfreire@gmail.com>
2020-08-17 09:14:37 -0300

    05.03 - Parâmetros opcionais

--- app/js/views/View.js
+++ app/js/views/View.js
@@ -6,11 +6,16 @@ System.register([], function (exports_1, context_1) {
         setters: [],
         execute: function () {
             View = class View {
-                constructor(seletor) {
+                constructor(seletor, escapar) {
+                    this.escapar = escapar;
                     this._elemento = $(seletor);
                 }
                 update(modelo) {
-                    this._elemento.html(this.template(modelo));
+                    let template = this.template(modelo);
+                    if (this.escapar) {
+                        template = template.replace(/<script>[\s\S]*?<\/script>/, '');
+                    }
+                    this._elemento.html(template);
                 }
             };
             exports_1("View", View);
--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -2,12 +2,16 @@ export abstract class View<T> {
 
     private _elemento:JQuery;
 
-    constructor(seletor:string){
+    constructor(seletor:string, private escapar?:boolean){
         this._elemento = $(seletor);
     }
 
-    update(modelo:T):void{
-        this._elemento.html(this.template(modelo));
+    update(modelo:T):void {
+        let template = this.template(modelo);
+        if (this.escapar){
+            template = template.replace(/<script>[\s\S]*?<\/script>/, '');
+        }
+        this._elemento.html(template);
     }
 
     abstract template(modelo:T): string;
