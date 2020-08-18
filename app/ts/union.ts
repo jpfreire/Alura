a5b7d86ffab102fc5b0ec894ec5a38ed79857f2e
Joao Paulo Freire <jpfreire@gmail.com>
2020-08-18 16:59:45 -0300

    05.11 - Type alias

--- app/ts/union.ts
+++ app/ts/union.ts
@@ -1,14 +1,18 @@
-function processaToken(token: string | number) {
+// criando o alias!
+type MeuToken = string |  number;
+
+function processaToken(token: MeuToken) {
 
     if(typeof(token) === 'string') {
 
-        // typescript entende que é o tipo string e faz autocomplete para este tipo. A função replace só existe em string
         return token.replace(/2/g,'X');
     } else {
-        // toFixed só existe em number!
+
         return token.toFixed().replace(/2/g,'X');
     }
+
 }
+
 
 const tokenProcessado1 = processaToken('1234');
 const tokenProcessado2 = processaToken(1234);
