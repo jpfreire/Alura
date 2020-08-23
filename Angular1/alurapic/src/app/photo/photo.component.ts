88b9b21a5b1465809fbe4dfc438c267d6b8b3cf1
Joao Paulo Freire <jpfreire@gmail.com>
2020-08-23 17:31:54 -0300

    02.03 - Tornando uma classe um componente

--- Angular1/alurapic/src/app/photo/photo.component.ts
+++ Angular1/alurapic/src/app/photo/photo.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'ap-photo',
-    templateUrl: 'photo.component.html'
+    templateUrl: './photo.component.html'
 })
 export class PhotoComponent {
     descricao = 'leão/leoa';
